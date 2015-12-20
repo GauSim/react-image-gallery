43693f08bbcb7490bebb92d8786a8e30715b2b1b
Simon Gausmann <Simon.Gausmann@live.de>
2015-12-20 11:21:09 +0100

    dist

--- gruntfile.js
+++ gruntfile.js
@@ -11,6 +11,46 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-systemjs-builder");
 	grunt.registerTask("default", ['bundle']);
 
+	grunt.registerTask("test", ['bundle2']);
+
+	grunt.registerTask("bundle2", "jspm bundling into SFX", function () {
+	
+		// see more details on that here: https://github.com/jspm/jspm-cli/blob/master/docs/api.md
+		var done = this.async();
+
+		var jspm = require('jspm');
+		jspm.setPackagePath('.');
+
+		var builder = new jspm.Builder();
+		var swig = require('swig');
+		var template = swig.compileFile('./index_full.html');
+		var http = require('http');
+		
+		// in momory build
+		builder.bundle("app", {
+			minify: true,
+			mangle: true,
+			lowResSourceMaps: true,
+			sourceMaps: false
+		})
+			.then(function (jsFile) {
+				console.log(jsFile);
+
+				// Create an HTTP server
+				var srv = http.createServer(function (req, res) {
+					res.writeHead(200, { 'Content-Type': 'text/html' });
+					res.end(template({ render: jsFile }));
+				});
+				srv.listen(9999);
+				//done();
+			})
+			.catch(function (err) {
+				console.log(err);
+				done(err);
+			});
+
+	});
+
 	grunt.registerTask("bundle", "jspm bundling into SFX", function () {
 
 		// see more details on that here: https://github.com/jspm/jspm-cli/blob/master/docs/api.md
@@ -32,6 +72,6 @@ module.exports = function (grunt) {
 			grunt.log.writeln(error);
 			done(error);
 		});
-		
+
 	});
 };
